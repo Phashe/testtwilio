0d2e97cec0c8d9fddba86bb8cf013b639e22962e
Phashe <12390na@gmail.com>
2024-02-20 13:29:36 +0200

    added main.go file

---
+++ go.mod
@@ -0,0 +1,13 @@
+module twilio
+
+go 1.22.0
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/gorilla/schema v1.2.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sfreiberg/gotwilio v1.0.0 // indirect
+	github.com/twilio/twilio-go v1.18.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/twilio/twilio-go"
+	api "github.com/twilio/twilio-go/rest/api/v2010"
+)
+
+func main() {
+	// Find your Account SID and Auth Token at twilio.com/console
+	// and set the environment variables. See http://twil.io/secure
+	client := twilio.NewRestClient()
+
+	params := &api.CreateMessageParams{}
+	params.SetFrom("whatsapp:+14155238886")
+	params.SetBody("Hello there!")
+	params.SetTo("whatsapp:+27649015322")
+
+	resp, err := client.Api.CreateMessage(params)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		if resp.Sid != nil {
+			fmt.Println(*resp.Sid)
+		} else {
+			fmt.Println(resp.Sid)
+		}
+	}
+}
